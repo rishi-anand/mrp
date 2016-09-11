9da8b4562c438a1fef379bd85c75e40f6186b97b
RISHI ANAND <rishi.anand0@gmail.com>
2016-09-12 00:55:59 +0530

    first code push

---
+++ application/controllers/Login.php
@@ -0,0 +1,54 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller 
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		if($this->Employee->is_logged_in())
+		{
+			redirect('home');
+		}
+		else
+		{
+			$this->form_validation->set_rules('username', 'lang:login_undername', 'callback_login_check');
+    	    $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+			
+			if($this->form_validation->run() == FALSE)
+			{
+				$this->load->view('login');
+			}
+			else
+			{
+				if($this->config->item('statistics') == TRUE)
+				{
+					$this->load->library('tracking_lib');
+
+					$login_info = $this->config->item('language') . ' | ' . $this->config->item('timezone') . ' | ' . $this->config->item('currency_symbol') . ' | ' . $this->config->item('theme') . ' | ' . $this->config->item('website') . ' | ' . $this->input->ip_address();
+					$this->tracking_lib->track_page('login', 'login', $login_info);
+				}
+
+				redirect('home');
+			}
+		}
+	}
+
+	public function login_check($username)
+	{
+		$password = $this->input->post('password');	
+
+		if(!$this->Employee->login($username, $password))
+		{
+			$this->form_validation->set_message('login_check', $this->lang->line('login_invalid_username_and_password'));
+
+			return FALSE;
+		}
+
+		return TRUE;		
+	}
+}
+?>
---
+++ application/helpers/dompdf_helper.php
@@ -0,0 +1,29 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+function pdf_create($html, $filename = '', $stream = TRUE) 
+{
+    require_once(APPPATH . 'helpers/dompdf/autoload.inc.php');
+
+	// need to enable magic quotes for the 
+	$magic_quotes_enabled = get_magic_quotes_runtime();
+    if(!$magic_quotes_enabled)
+    {
+    	ini_set("magic_quotes_runtime", true);
+    }
+
+    $dompdf = new Dompdf\Dompdf();
+    $dompdf->loadHtml($html);
+    $dompdf->render();
+
+    ini_set("magic_quotes_runtime", $magic_quotes_enabled);
+
+    if ($stream)
+	{
+        $dompdf->stream($filename.".pdf");
+    }
+	else
+	{
+        return $dompdf->output();
+    }
+}
+?>  
