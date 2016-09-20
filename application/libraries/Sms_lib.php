dfcecad052b3574221673dafabd2e0619a7e425c
RISHI ANAND <rishi.anand0@gmail.com>
2016-09-20 23:52:12 +0530

    sending sms to one person at a time, to add more validation, to support multiple number

--- application/libraries/Sms_lib.php
+++ application/libraries/Sms_lib.php
@@ -13,31 +13,31 @@ class Sms_lib
 	 * SMS sending function
 	 * Example of use: $response = sendSMS('4477777777', 'My test message');
 	 */
-	public function sendSMS($phone, $message)
+	public function sendSMS($numbers, $message)
 	{
 		$username   = $this->CI->config->item('msg_uid');
-		$password   = $this->CI->config->item('msg_pwd');
-		$originator = $this->CI->config->item('msg_src');
+		$hash   = $this->CI->config->item('msg_pwd');
+		$sender = $this->CI->config->item('msg_src');
 		
 		$response = FALSE;
 		
 		// if any of the parameters is empty return with a FALSE
-		if(empty($username) || empty($password) || empty($phone) || empty($message) || empty($originator))
+		if(empty($username) || empty($hash) || empty($numbers) || empty($message) || empty($sender))
 		{
-			//echo $username . ' ' . $password . ' ' . $phone . ' ' . $message . ' ' . $originator;
+			//echo $username . ' ' . $hash . ' ' . $numbers . ' ' . $message . ' ' . $sender;
 		}
 		else
 		{
 			$response = TRUE;
 			
 			// make sure passed string is url encoded
-			$message = rawurlencode($message);
+			//-----$message = rawurlencode($message);
 			
 			// add call to send a message via 3rd party API here
 			// Some examples
 
 			/*
-			$url = "http://xxx.xxx.xxx.xxx/send_sms?username=$username&password=$password&src=$originator&dst=$phone&msg=$message&dr=1";
+			$url = "http://xxx.xxx.xxx.xxx/send_sms?username=$username&password=$hash&src=$sender&dst=$numbers&msg=$message&dr=1";
 			 
 			$c = curl_init(); 
 			curl_setopt($c, CURLOPT_RETURNTRANSFER, 1); 
@@ -48,11 +48,38 @@ class Sms_lib
 			
 			// This is a textmarketer.co.uk API call, see: http://wiki.textmarketer.co.uk/display/DevDoc/Text+Marketer+Developer+Documentation+-+Wiki+Home
 			/*
-			$url = 'https://api.textmarketer.co.uk/gateway/'."?username=$username&password=$password&option=xml";
-			$url .= "&to=$phone&message=".urlencode($message).'&orig='.urlencode($originator);
+			$url = 'https://api.textmarketer.co.uk/gateway/'."?username=$username&password=$hash&option=xml";
+			$url .= "&to=$numbers&message=".urlencode($message).'&orig='.urlencode($sender);
 			$fp = fopen($url, 'r');
 			$response = fread($fp, 1024);
 			*/
+				// Authorisation details.
+			
+
+			//textlocal
+			
+			//$username = "<username>";
+			//$hash = "<hash_key>";
+
+			// Config variables. Consult http://api.textlocal.in/docs for more info.
+			// $test = "1"; //testing (no sms is sent to user)
+			// $test = "0"; //production (sms is sent to user)
+			$test = "0";
+
+			// Data for text message. This is the text message data.
+			//$sender = "TXTLCL"; // This is who the message appears to be from.
+			//$numbers = "918888888888"; // A single number or a comma-seperated list of numbers
+			//$message = "This is a test message from the PHP API script. -Rishi Anand";
+			// 612 chars or less
+			// A single number or a comma-seperated list of numbers
+			$message = urlencode($message);
+			$data = "username=".$username."&hash=".$hash."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
+			$ch = curl_init('http://api.textlocal.in/send/?');
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$result = curl_exec($ch); // This is the result from the API
+			curl_close($ch);
 		}
 
 		return $response;
