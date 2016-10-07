51502a6cbca09aff043bc43846a808d5cb1b52ac
RISHI ANAND <rishi.anand0@gmail.com>
2016-10-08 03:20:22 +0530

    removing black screen

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -2,7 +2,7 @@
 	</div>
 
 	<div id="footer">
-		<div class="jumbotron push-spaces">
+		<div class="jumbotron push-spaces" style="display:none;">
 			<strong><?php echo $this->lang->line('common_you_are_using_ospos'); ?>
   			<?php echo $this->config->item('application_version'); ?> - <?php echo substr('$Id: 4f5ad5761503bbc94152cbf16afa76439ca155b8 $', 5, 7); ?></strong>.
 			<?php echo $this->lang->line('common_please_visit_my'); ?>
@@ -10,5 +10,25 @@
 			<?php echo $this->lang->line('common_learn_about_project'); ?>
 		</div>
 	</div>
+
+	<div id="footer" style="position:fixed;font-size:11px;color:#777;clear:both;bottom:0;left:0;height:40px;width:100%;">
+		<div class="jumbotron push-spaces">
+			<center><strong>MRP Solutions</strong></center>
+		</div>
+	</div>
+
+	<script>
+	var t = 0;
+		var x = setTimeout(function(){
+			$.unblockUI();
+			var y = setTimeout(function(){
+			$.unblockUI();
+			clearTimeout(y);
+		},50);
+			clearTimeout(x);
+		},50);
+
+		
+	</script>
 </body>
 </html>
